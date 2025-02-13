2052bcf96d7ba3b3f3c3709a4597640eefb7974d
AMINE7119 <amine71198@gmail.com>
2025-02-13 20:28:48 +0100

    100%

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,3 @@
-from flask import Flask, render_template
-from app.controllers.auth_controller import auth_bp
-from app.controllers.bank_controller import bank_bp
-from app.controllers.analytics_controller import analytics_bp
-from app.errors.error import register_error_handlers
-import secrets
+from flask import Flask
 
-def create_app():
-    app = Flask(__name__)
-    app.secret_key = secrets.token_hex(32)
-
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(bank_bp, url_prefix='/bank')
-    app.register_blueprint(analytics_bp, url_prefix='/analytics')
-    register_error_handlers(app)
-
-    @app.route('/')
-    def index():
-        return render_template('auth/login.html')
-
-    return app
+app = Flask(__name__)
--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-from app import create_app
-
-app = create_app()
+from flask import Flask, render_template, Blueprint
+from app.controllers.auth_controller import auth_bp
+from app.controllers.bank_controller import bank_bp
+from app.controllers.analytics_controller import analytics_bp
+from app import app
+from app.errors.error import register_error_handlers
+import secrets
 
 if __name__ == '__main__':
+    auth = app.register_blueprint(auth_bp)
+    bank = app.register_blueprint(bank_bp, url_prefix='/bank')
+    analytics = app.register_blueprint(analytics_bp, url_prefix='/analytics')
+    register_error_handlers(app)
+    app.secret_key = secrets.token_hex(32)
+    
+    @app.route('/')
+    def index():
+        return render_template('auth/login.html')
+        
     app.run(debug=True, host='0.0.0.0', port=5000)
